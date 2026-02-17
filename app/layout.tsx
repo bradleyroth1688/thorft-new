3bb5c1e5e9fa58050341183da544265f7933a5c2
Bradley Roth <bradleyroth@Bradleys-MacBook-Pro.local>
2026-02-17 15:35:22 -0500

    Add LinkedIn Insight Tag for retargeting (partner ID: 86s9628nsfk0o3)

--- app/layout.tsx
+++ app/layout.tsx
@@ -69,6 +69,24 @@ export default function RootLayout({
             gtag('config', 'G-MWXP9F3QTC');
           `}
         </Script>
+        <Script id="linkedin-insight" strategy="afterInteractive">
+          {`
+            _linkedin_partner_id = "86s9628nsfk0o3";
+            window._linkedin_data_partner_ids = window._linkedin_data_partner_ids || [];
+            window._linkedin_data_partner_ids.push(_linkedin_partner_id);
+            (function(l) {
+              if (!l){window.lintrk = function(a,b){window.lintrk.q.push([a,b])};
+              window.lintrk.q=[]}
+              var s = document.getElementsByTagName("script")[0];
+              var b = document.createElement("script");
+              b.type = "text/javascript";b.async = true;
+              b.src = "https://snap.licdn.com/li.lms-analytics/insight.min.js";
+              s.parentNode.insertBefore(b, s);})(window.lintrk);
+          `}
+        </Script>
+        <noscript>
+          <img height="1" width="1" style={{display: "none"}} alt="" src="https://px.ads.linkedin.com/collect/?pid=86s9628nsfk0o3&fmt=gif" />
+        </noscript>
       </head>
       <body className={`${inter.className} antialiased`}>
         <Header />
